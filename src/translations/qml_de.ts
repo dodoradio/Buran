6cb5fb1059c7d42366dbb9a00b56dbca0b1f1352
Ed Beroset <beroset@ieee.org>
2022-03-31 23:37:19 -0400

    Rename translation files

---
+++ src/translations/qml_de.ts
@@ -0,0 +1,119 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de">
+<context>
+    <name>InfoPage</name>
+    <message>
+        <location filename="../qml/pages/InfoPage.qml" line="37"/>
+        <source>About Starship</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/InfoPage.qml" line="52"/>
+        <source>Version %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/InfoPage.qml" line="65"/>
+        <source>Legal</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MainMenuPage</name>
+    <message>
+        <location filename="../qml/pages/MainMenuPage.qml" line="113"/>
+        <source>enable time synchronisation</source>
+        <translation>Zeit Sync</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainMenuPage.qml" line="168"/>
+        <source>notification settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainMenuPage.qml" line="179"/>
+        <source>Telescope</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainMenuPage.qml" line="179"/>
+        <source>Watch-Finder</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainMenuPage.qml" line="179"/>
+        <source>The phone is looking for you!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainMenuPage.qml" line="222"/>
+        <source>find my watch</source>
+        <translation>Find mein Uhr</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainMenuPage.qml" line="277"/>
+        <source>take a screenshot</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainMenuPage.qml" line="332"/>
+        <source>weather settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>NotificationSettingsPage</name>
+    <message>
+        <location filename="../qml/pages/NotificationSettingsPage.qml" line="14"/>
+        <source>None</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/NotificationSettingsPage.qml" line="15"/>
+        <source>Normal</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/NotificationSettingsPage.qml" line="16"/>
+        <source>Strong</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>WatchSelectionPage</name>
+    <message>
+        <location filename="../qml/pages/WatchSelectionPage.qml" line="78"/>
+        <source>Open Bluetooth Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>WeatherSettingsPage</name>
+    <message>
+        <location filename="../qml/pages/WeatherSettingsPage.qml" line="45"/>
+        <source>Locations</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/WeatherSettingsPage.qml" line="96"/>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/WeatherSettingsPage.qml" line="103"/>
+        <source>Save Changes</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/WeatherSettingsPage.qml" line="178"/>
+        <source>Add Location</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/WeatherSettingsPage.qml" line="187"/>
+        <source>Current Location</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
